2b6167726a966dcc1a3269abdeaa25e651cf1f15
Umaiyal Sridas <umaiyal.sridas@gmail.com>
2024-03-05 11:39:30 -0800

    move index to root

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import reportWebVitals from './src/reportWebVitals';
 import App from './App';
 import './src/fonts/EditorialNew/PPEditorialNew-Italic.otf';
 import './src/fonts/EditorialNew/PPEditorialNew-Regular.otf';
@@ -9,6 +8,7 @@ import './src/fonts/EditorialNew/PPEditorialNew-Ultrabold.otf';
 import './src/fonts/EditorialNew/PPEditorialNew-UltraboldItalic.otf';
 import './src/fonts/EditorialNew/PPEditorialNew-Ultralight.otf';
 import './src/fonts/EditorialNew/PPEditorialNew-UltralightItalic.otf';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
