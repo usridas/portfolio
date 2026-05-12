d86dcdba7a5abdf3212fed4e9a319d3a949a7cb4
usridas <umaiyal.sridas@gmail.com>
2026-05-12 11:45:56 -0700

    fix vinyl issue

--- src/pages/Vinyl.js
+++ src/pages/Vinyl.js
@@ -33,36 +33,38 @@ export const Vinyl = ({songCover, vinylAnimation, isPaused}) => {
     </div>
 
     return (
-        <div className={vinylAnimation} style={{animationPlayState: `${isPaused ? 'paused': 'running'}`}}>
-            <div className='vinylLiquidGlassWrapper'>
-                <div className='vinylLiquidGlassEffect'></div>
-                <div className='centerImage'>
-                    {layeredVinyl}
+        <div className='vinylViewport'>
+            <div className={vinylAnimation} style={{animationPlayState: `${isPaused ? 'paused': 'running'}`}}>
+                <div className='vinylLiquidGlassWrapper'>
+                    <div className='vinylLiquidGlassEffect'></div>
+                    <div className='centerImage'>
+                        {layeredVinyl}
+                    </div>
+                    <svg>
+                        <filter id='glassDistortion'>
+                            <feTurbulence
+                                type='fractalNoise'
+                                baseFrequency='0.002 0.002'
+                                numOctaves='1'
+                                result='turbulence'
+                            />
+                            <feDisplacementMap
+                                in='SourceGraphic'
+                                scale='200'
+                                result='displacement'
+                            />
+                            <feSpecularLighting
+                                result="spotlight"
+                                specularConstant="0.02"
+                                specularExponent="1000"
+                                lighting-color="white">
+                                <fePointLight x="150" y="150" z="20" />
+                            </feSpecularLighting>
+                            <feComposite
+                                in="displacement" in2="spotlight" operator="lighter" result="composited"/>
+                        </filter>
+                    </svg>
                 </div>
-                <svg>
-                    <filter id='glassDistortion'>
-                        <feTurbulence
-                            type='fractalNoise'
-                            baseFrequency='0.002 0.002'
-                            numOctaves='1'
-                            result='turbulence'
-                        />
-                        <feDisplacementMap
-                            in='SourceGraphic'
-                            scale='200'
-                            result='displacement'
-                        />
-                        <feSpecularLighting
-                            result="spotlight"
-                            specularConstant="0.02"
-                            specularExponent="1000"
-                            lighting-color="white">
-                            <fePointLight x="150" y="150" z="20" />
-                        </feSpecularLighting>
-                        <feComposite
-                            in="displacement" in2="spotlight" operator="lighter" result="composited"/>
-                    </filter>
-                </svg>
             </div>
         </div>
     );
