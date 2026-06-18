0d191487e79be2a4253c6f25ef5d23fb8fa66fd1
usridas <umaiyal.sridas@gmail.com>
2026-06-18 09:10:20 -0700

    animations fix 4

--- src/components/Grid/ProjectGrid.js
+++ src/components/Grid/ProjectGrid.js
@@ -2,20 +2,23 @@ import './ProjectGrid.scss';
 import { useScreenResolution } from '../../utils/ScreenSize.tsx';
 import Button from '../Button/Button.js';
 import Resume from '../../assets/documents/Resume.pdf';
+import { useEffect, useRef } from 'react';
 
 export const ProjectGrid = ({gridProps, removeBio=false}) => {
     const { isLarge, isMedium } = useScreenResolution();
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        containerRef.current?.classList.add('projectGrid-animated');
+    }, []);
 
     return (
-        <div className='projectGrid-gridContainer'>
+        <div className='projectGrid-gridContainer' ref={containerRef}>
             {(isLarge || isMedium) && !removeBio &&
             <div className='projectGrid-rowOuter'>
                 <div className='projectGrid-stickyRow'>
                     <h1 className='projectGrid-leftCol'>{'ABOUT ME'}</h1>
-                    <div
-                        className='projectGrid-rightCol'
-                        
-                    >
+                    <div className='projectGrid-rightCol'>
                         <p style={{marginBottom: '24px'}}>{'After graduating from my undergrad, I moved to San Diego, California to start work as a software engineer. Through work, I was able to explore my love for development, design, and management. I love the intersection between art and technology and aim to be a bridge between these two fields.\n\n​I am currently in a Masters program for User Experience Design and am searching for work in the UI/UX field.'}</p>
                         <Button type='Primary' text='VIEW MY RESUME' link={Resume}/>
                     </div>
