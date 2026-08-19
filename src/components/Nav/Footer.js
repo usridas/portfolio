21591789f6c4c61c37c927cfeaddbbf8e1073117
usridas <umaiyal.sridas@gmail.com>
2026-08-19 10:51:12 -0700

    cursor fix

--- src/components/Nav/Footer.js
+++ src/components/Nav/Footer.js
@@ -8,12 +8,12 @@ export const Footer = () => {
     <footer className='footerContainer'>
       <div className='footer'>
         {!isMobile && <div/>}
-        <h1 style={{color: '#fdfcf8', fontSize: isMobile ? '24px' : '40px'}}>Let's make cute things!</h1>
-        <div/>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <h1 style={{color: '#fdfcf8', fontSize: isMobile ? '24px' : '40px', gridColumn: isMobile ? 'span 2' : ''}}>Let's make cute things!</h1>
+        {!isMobile && <div/>}
+        <div style={{display: 'flex', flexDirection: 'column', gridColumn: isMobile ? 'span 2' : ''}}>
           <p style={{color: '#fdfcf8'}}>Contact me</p>
           <a tabIndex={0} className='link hover-target' href="mailto:umaiyal.sridas@gmail.com" style={{color: '#fdfcf8', marginBottom: '24px'}}>umaiyal.sridas@gmail.com</a>
-          <p style={{color: '#fdfcf8'}}>Website designed and developed by Umaiyal Sridas. Last updated 16 August 2026.</p>
+          <p style={{color: '#fdfcf8'}}>Website designed and developed by Umaiyal Sridas. Last updated 19 August 2026.</p>
         </div>
       </div>
     </footer>
